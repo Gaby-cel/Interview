a06998d8b2869ce8d32385f9f966bd17d8a62c7d
Gaby-cel <gabriela109428@mcpsva.org>
2025-10-20 14:18:26 -0400

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,52 @@
+from ftplib import parse227
+
+
+"""
+Filename: madlibs.py
+Author: <Guardado, Gabriela>
+Created: <09/03/2025>
+Instructor: Holtslander
+"""
+
+
+p1 = input("What is person 1's name?\n")
+p2 = input("Please enter any video game character\n")
+p3 = input("Please enter any adjective\n")
+p4 = input("Please enter any movie or show\n")
+p5 = input("Please enter any edible item\n")
+p6 = input("Please enter a noun\n")
+p7 = input("Please name any shopping place\n")
+p8 = input("Please enter a noun\n")
+p9 = input("Please enter a noun\n")
+p10 = input("Please enter an important announcement\n")
+p11 = input("Please enter a noun\n")
+p12 = input("Please enter any topic\n")
+p13 = input("Please enter an adjective\n")
+p14 = input("Please enter a place\n")
+p15 = input("Please enter the name of a dinner item\n")
+p16 = input("Please enter the name of something edible\n")
+p17 = input("Please enter a noun\n")
+p18 = input("Please enter the name of a show\n")
+p19 = input("Please enter a noun\n")
+p20 = input("Please enter an event\n")
+
+
+
+
+print (f"{p1} was 32 and lived in a cramped apartment that could barely fit their {p2} collection. Every morning,\n"
+       f"they woke up to the blaring sound of their {p3} alarm clock, immediately regretting staying up too\n"
+       f"late watching {p4}. After a hasty breakfast of {p5}, they grabbed their {p6} and begrudgingly headed to work.\n")
+
+print (f"The walk to the office was the same as always: pass the {p7} shop, dodge the usual {p8} at the bus stop,\n "
+       f"and try to avoid eye contact with the {p9} street performer who still thought it was 1998.\n")
+
+print (f"At the office, the day began with an email from their boss titled \"{p10}\") (followed by endless {p11},\n"
+       f" including a meeting about {p12}. Lunchtime was spent eating a {p13} sandwich while\n "
+       "pretending to be productive on their laptop.\n")
+
+print (f"After work, {p1} stopped by the {p14} for something totally unnecessary, then rushed home to make a {p15}\n"
+       f" for dinner that somehow turned into {p16}. The apartment was so quiet that even the {p17} seemed to be napping.\n")
+
+print(f"Evenings were spent binge-watching {p18} and thinking about that book, {p19}, they bought years ago. They always\n"
+      " promised they’d read it… eventually. As they lay in bed, ready to repeat the whole thing tomorrow, they thought,\n"
+      f" “Maybe tomorrow will be different.” And then {p20} happened.\n")
